4c49b4e5fc685eceec387b71a4f5d0ca80f96085
Josef Hak <josef.hak@booking.com>
2023-12-24 02:06:17 +0100

    add media files, add file description

--- 07_car_commands.py
+++ 07_car_commands.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env pybricks-micropython
 
 # Naprogramuj robota pomocí barev, jak má jet
-
-# S1 - gyro
-# S3 - tlačítko
-# S4 - ultrazvuk (oči)
-
+# cerveným tlačítkem potvrzuj příkazy (barvy)
+# zeleným tlačítkem robota odstartuj
 
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
@@ -19,8 +16,6 @@ from pybricks.robotics import DriveBase
 # Initialize the EV3 Brick.
 ev3 = EV3Brick()
 
-ev3.speaker.beep()
-
 left_motor = Motor(Port.B)
 right_motor = Motor(Port.C)
 wheel_diameter = 55.5
@@ -33,7 +28,8 @@ touch_sensor_read = TouchSensor(Port.S2) # right one
 color_sensor = ColorSensor(Port.S3)
 distance_sensor = UltrasonicSensor(Port.S4)
 
-ev3.speaker.say("Hello!")
+ev3.speaker.play_file("sound/motor_start.wav")
+ev3.screen.load_image("img/neutral.png")
 
 def update_colors(colors, color):
     "Update colors array, aggregate values of the same type"
@@ -48,35 +44,42 @@ def go_to_wall(speed):
         while True:
             if distance_sensor.distance() < 100:
                 driveBase.stop()
+                ev3.screen.load_image("img/angry.png")
+                ev3.speaker.play_file("sound/horn_1.wav")
                 break
 
 def try_get_commands(prev_colors):
     colors = []
     while True:
         if touch_sensor_go.pressed():
+            ev3.speaker.play_file("sound/motor_start.wav")
             if colors == []:
-                ev3.speaker.say("Once again")
                 return prev_colors
-            ev3.speaker.say("Program saved")
             return colors
         if not touch_sensor_read.pressed():
             continue
         c = color_sensor.color()
         print(c)
         if c == Color.RED:
-            ev3.speaker.say("red, rights")
+            # ev3.speaker.say("red, rights")
+            ev3.screen.load_image("img/middle_right.png")
+            ev3.speaker.play_file("sound/cervena_vpravo.wav")
             update_colors(colors, [c, -90])
         elif c == Color.BLUE:
-            ev3.speaker.say("blue, left")
+            ev3.screen.load_image("img/middle_left.png")
+            ev3.speaker.play_file("sound/modra_vlevo.wav")
             update_colors(colors, [c, 90])
         elif c == Color.GREEN:
-            ev3.speaker.say("green, forward")
+            ev3.screen.load_image("img/up.png")
+            ev3.speaker.play_file("sound/zelena_vpred.wav")
             update_colors(colors, [c, 100])
         elif c == Color.BLACK:
-            ev3.speaker.say("black, back")
+            ev3.screen.load_image("img/tired_middle.png")
+            ev3.speaker.play_file("sound/cerna_zpet.wav")
             update_colors(colors, [c, -100])
         elif c == Color.WHITE:
-            ev3.speaker.say("white, go to wall")
+            ev3.screen.load_image("img/up.png")
+            ev3.speaker.play_file("sound/bila_stale_vpred.wav")
             update_colors(colors, [c, 100])
         # elif c == Color.YELLOW:
         #     ev3.speaker.say("white, go to distance")
@@ -95,22 +98,28 @@ while True:
         continue
     for c, v in colors:
         if c == Color.RED:
-            ev3.speaker.say("right")
+            ev3.screen.load_image("img/middle_right.png")
+            ev3.speaker.play_file("sound/vpravo.wav")
             driveBase.turn(angle=v)
         elif c == Color.BLUE:
-            ev3.speaker.say("left")
+            ev3.screen.load_image("img/middle_left.png")
+            ev3.speaker.play_file("sound/vlevo.wav")
             driveBase.turn(angle=v)
         elif c == Color.GREEN:
-            ev3.speaker.say("forward")
+            ev3.screen.load_image("img/up.png")
+            ev3.speaker.play_file("sound/vpred.wav")
             driveBase.straight(distance=v)
         elif c == Color.BLACK:
-            ev3.speaker.say("back")
+            ev3.screen.load_image("img/tired_middle.png")
+            # ev3.speaker.play_file("sound/zpet.wav")
+            ev3.speaker.play_file("sound/backing_alert.wav")
             driveBase.straight(distance=v)
         elif c == Color.WHITE:
-            ev3.speaker.say("go to wall")
+            ev3.screen.load_image("img/up.png")
+            ev3.speaker.play_file("sound/stale_vpred.wav")
             go_to_wall(speed=v)
         # elif c == Color.YELLOW:
         #     ev3.speaker.say("yellow placeholder")
         # elif c == Color.BROWN:
         #     ev3.speaker.say("brown placeholder")
-    ev3.speaker.say("I am here!")
+    ev3.speaker.play_file("sound/fanfare.wav")
